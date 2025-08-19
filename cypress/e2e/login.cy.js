8788693a20bc8370917163d2b06e21e727bc9386
albed13 <albedsaputra91@gmail.com>
2025-08-19 08:21:08 +0700

    Update login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,1 +1,26 @@
-testing menguji coba
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Agar error 'year' atau 'F is not defined' tidak membuat test gagal
+    return false;
+  });
+describe('Login Growin Travel', () => {
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        if (
+            err.message.includes('fs is not defined') ||
+            err.message.includes('Identifier \'year\' has already been declared')
+          ) {
+            return false
+          }
+          
+        })
+
+it('should login with valid credentials', () => {
+    cy.visit('https://growintravel.com/login');
+    //login
+    cy.get('#login-form > :nth-child(1) > input', { timeout: 10000 }).type('admin123@gmail.com');
+    cy.get('#login-password').type('Admin@123')
+    cy.get('#login-btn').click()
+    cy.get('.swal2-confirm').click()
+
+})
+})
